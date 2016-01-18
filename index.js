220b296c1633c7b3593c9d7ab45a98c48ef07165
Srinath Janakiraman <vjsrinath@live.com>
2016-01-18 16:17:04 +0000

    Added models to save signup details

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const Ejs = require('ejs');
 const Good = require('good');
 const _ = require('underscore');
 const KongApiSyncPlugin = require('./plugins/kong-api-sync');
+const AccountModel = require("./model/Account");
 
 const server = new Hapi.Server();
 
@@ -41,7 +42,16 @@ server.register(Vision, (err) => {
         path: '/',
         handler: function(request, reply) {
             reply.view('signup', {
-                title: 'Welcome to ludicrum'
+                title: 'Welcome to ludicrum',
+                errors: [],
+                messages: [],
+                model: {
+                    username: '',
+                    password: '',
+                    email: '',
+                    firstName: '',
+                    lastName: ''
+                }
             });
         }
     });
@@ -60,7 +70,16 @@ server.register(Vision, (err) => {
         handler: function(request, reply) {
             //reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
             reply.view('signup', {
-                title: 'Welcome to ludicrum'
+                title: 'Welcome to ludicrum',
+                errors: [],
+                messages: [],
+                model: {
+                    username: '',
+                    password: '',
+                    email: '',
+                    firstName: '',
+                    lastName: ''
+                }
             });
         }
     });
@@ -70,9 +89,43 @@ server.register(Vision, (err) => {
         path: '/signup',
         handler: function(request, reply) {
             //reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
-            reply.view('signup', {
-                title: 'Welcome to ludicrum'
-            });
+            let input = request.payload;
+            let errors = [];
+
+            AccountModel.findOne({
+                    username: input.username.toLowerCase()
+                })
+                .then(account => {
+                    //if account already exists, then throw exception saying username is already taken
+                    if (account) {
+                        throw new Error('Username already taken.');
+                    }
+
+                    var model = new AccountModel(input);
+
+                    return model
+                        .save()
+                        .then(newAcc => {
+                            reply.view('signup', {
+                                title: 'Welcome to ludicrum',
+                                messages: ["Account created successfully"],
+                                errors: errors,
+                                model: input
+                            });
+                        });
+
+                })
+                .catch(e => {
+                    errors.push(e.message);
+                    reply.view('signup', {
+                        title: 'Welcome to ludicrum',
+                        errors: errors,
+                        model: input
+                    });
+                });
+
+
+
         }
     });
 
